863b08ddde7393482e715a3e326615e1b66a68c0
EIK <petrodarchyn@gmail.com>
2019-08-08 21:39:55 +0300

    microservice registration

--- src/main/java/com/plainprog/dictionary/DictionaryApplication.java
+++ src/main/java/com/plainprog/dictionary/DictionaryApplication.java
@@ -2,8 +2,10 @@ package com.plainprog.dictionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DictionaryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DictionaryApplication.class, args);
